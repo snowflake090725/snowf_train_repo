08efa3ce8613b898faf0bb987c5b505208c1a22a
sganesh <snowflake250824@gmail.com>
2025-09-17 07:08:29 +0000

    adding a print statement to get table data

--- create_table.py
+++ create_table.py
@@ -5,6 +5,7 @@ import json
 def main(session: snowpark.Session):
     data = get_data_from_stage(session, 'train', 'snowflake_train_stage')
     table_data = get_dataframe(session, data)
+    print(table_data)
     create_table(session, 'HOUSE_PRICING', table_data)
     return table_data
     
